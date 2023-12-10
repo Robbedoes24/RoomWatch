75f57518d00c51bf207094fcba072869ec94671f
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-12-10 17:12:39 +0100

    #4 Added inference script

---
+++ firmware/detection/detect.py
@@ -0,0 +1,136 @@
+import os
+import cv2
+import time
+import json
+from ultralytics import YOLO
+from dotenv import load_dotenv
+import paho.mqtt.client as mqtt
+
+# --- Load environment variables ---
+load_dotenv()
+
+# --- Setings ---
+MQTT_HOST = os.getenv("MQTT_HOST")
+MQTT_PORT = int(os.getenv("MQTT_PORT"))
+MQTT_TOPIC = os.getenv("MQTT_TOPIC")
+ROOM_NAME = os.getenv("ROOM_NAME")
+
+CAMERA_WIDTH = 1920
+CAMERA_HEIGHT = 1080
+CAMERA_FPS = 30
+
+DETECTION_THRESHOLD = 0.5 # 0.0 - 1.0 (confidence threshold)
+DETECTION_FREQUENCY = 5 # seconds (run detection every x seconds, make sure this is higher than the inference time)
+DETECTION_PUBLISH_FREQUENCY = 60 # seconds (publish detection average every x seconds, make sure this is a multiple of the DETECTION_FREQUENCY for consistent results)
+
+# --- Pre run config/setup ---
+# Load the YOLOv8 model
+model = YOLO('yolov8n.pt')
+
+# Create a camera object (for image capture)
+cap = cv2.VideoCapture(0)
+# Edit camera settings
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)   # Set width
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT) # Set height
+cap.set(cv2.CAP_PROP_FPS, CAMERA_FPS)             # Set FPS
+
+# Create a MQTT client
+client = mqtt.Client()
+
+# --- Functions ---
+def amountOfPeople(frame):
+  # Run the model on the frame
+  results = model(frame)
+
+  # Convert the results to json (for easier processing)
+  json_results = json.loads(results[0].tojson())
+
+  # Get the amount of people detected from the json results
+  amount_of_people_detected = 0
+  for result in json_results:
+    if result["name"] == "person" and round(result["confidence"],2) > DETECTION_THRESHOLD:
+      amount_of_people_detected += 1
+
+  # Return the amount of people detected
+  return amount_of_people_detected
+
+def calculateAverage(list):
+  # Calculate the average
+  sum = 0
+  for item in list:
+    sum += item
+  average = sum / len(list)
+
+  # Return the average as an integer
+  return round(average)
+
+def publishDetectionAverage(client, average, start_time):
+  # Create the payload
+  payload = {
+    "uptime": round(time.time() - start_time),
+    "people": average
+  }
+
+  # Publish the payload to the MQTT broker
+  client.publish(MQTT_TOPIC + "/" + ROOM_NAME, json.dumps(payload, indent=4))
+
+# --- Main ---
+def main():
+  # Connect to the MQTT broker
+  print("Connecting to MQTT broker")
+  client.connect(MQTT_HOST, MQTT_PORT)
+
+  # Start loop to run detection every x seconds
+  start_time = time.time()
+  last_detection_time = start_time
+  last_publish_time = start_time
+
+  # List with detected amounts
+  detection_amounts = []
+
+  print("Starting detection loop")
+  while True:
+    # Get the current time
+    current_time = time.time()
+
+    # If x seconds have passed since the last detection
+    if current_time - last_detection_time >= DETECTION_FREQUENCY:
+      # Reset the last detection time (before running detection, because it takes time)
+      last_detection_time = time.time()
+
+      # Read a frame from the camera
+      success, frame = cap.read()
+
+      if success:
+        # Get the amount of people detected
+        amount_of_people = amountOfPeople(frame)
+      else:
+        # Print error message
+        print("Camera malfunction")
+        # Send malfuction message to the MQTT broker
+        client.publish(MQTT_TOPIC + "/" + ROOM_NAME, "Error: Camera malfunction")
+        # Release the camera
+        cap.release()
+        # Close the program
+        exit()    
+      # Add the amount of people detected to the list
+      detection_amounts.append(amount_of_people)
+
+    # If x seconds have passed since the last publish
+    if current_time - last_publish_time >= DETECTION_PUBLISH_FREQUENCY:
+      # Reset the last publish time
+      last_publish_time = time.time()
+
+      # Calculate the average
+      average = calculateAverage(detection_amounts)
+
+      # Publish the average to the MQTT broker
+      print("\nPublishing average: " + str(average) + " people")
+      publishDetectionAverage(client, average, start_time)
+
+      # Clear the list
+      detection_amounts.clear()
+
+# --- Run main ---
+if __name__ == "__main__":
+  main()
