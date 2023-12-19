da4b4a1195c62b18bbada5da37c2da27ede49cd4
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-12-19 12:25:46 +0100

    Updated capture.py
    - Added a fix to frame buffer delay
    - Cleaned up code

--- firmware/data_acquisition/capture.py
+++ firmware/data_acquisition/capture.py
@@ -9,28 +9,32 @@ name_base = 'image'
 delay = 30 # seconds
 camera_width = 1920
 camera_height = 1080
+camera_fps = 1
 
 # Opens the Video file
 cap = cv2.VideoCapture(0)
 # Edit camera settings
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, camera_width)  # Set width
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, camera_width)   # Set width
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, camera_height) # Set height
+cap.set(cv2.CAP_PROP_FPS, camera_fps)             # Set FPS
 
 # Create the folder if it doesn't exist
 if not os.path.exists(folder):
     os.makedirs(folder)
 
 # Loop to capture images every x seconds
+last_capture_time = 0
 while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == False:
         break
-    # time in seconds since epoch (to add to the name)
-    current_time = round(time.time())
-    # save the image
-    cv2.imwrite(folder + name_base + str(current_time) + '.jpg', frame)
-    # wait x seconds
-    time.sleep(delay)
+    # Check if delay has passed
+    current_time = time.time()
+    if current_time - last_capture_time >= delay:
+        # save the image
+        cv2.imwrite(folder + name_base + str(round(current_time)) + '.jpg', frame)
+        # update last capture time
+        last_capture_time = current_time
     
 cap.release()
 cv2.destroyAllWindows()
